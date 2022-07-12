f0475381202cc38eaeb77189b67a85d354227e39
ming-chieh-liu <107483206+ming-chieh-liu@users.noreply.github.com>
2022-07-12 18:36:44 +0800

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,19 +1,25 @@
 # Write a function that replaces a string within a string and 
 # returns the new string
 
-def replace(original, replace, new):
+def replace(original, replace_word, new):
     """Docstring here"""
     #
     # Your code here
     #
-
-    return "something here"
-
-
+    length_of_word = len(replace_word)
+    try:
+        index = original.index(replace_word)
+    except:
+        return original
+    else:
+        new_word = original[:index] + new + original[index+length_of_word:]
+        output = replace(new_word, replace_word, new)
+        return output
 
 # Test your function
 greeting = "Today is Monday!"
 revised = replace(greeting, "Monday", "Funday")
+print(revised)
 
 # Test for failure
 assert revised != "Today is Monday!"
