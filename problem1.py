dec94a947420a79cb66238b0bcbf74d84df4827a
ming-chieh-liu <107483206+ming-chieh-liu@users.noreply.github.com>
2022-07-19 06:16:21 +0800

    Add files

--- problem1.py
+++ problem1.py
@@ -2,15 +2,33 @@
 # The function should return the sum. Ensure that that list 
 # contains only numbers. If not return the value `None`.
 
+def is_numbers(numbers):
+    """check whether the input is a list of numbers
+    input(list or tuple): a list of numbers
+    output(bool): if the type of the input is correct, return True; otherwise, return False
+    """
+    TF_numbers = False
+    # if the type of the input is a list and elements in the list are all numbers
+    if type(numbers) == list or type(numbers) == tuple:
+        TF_numbers = True
+        for number in numbers:
+            if type(number) != int and type(number) != float:
+                TF_numbers = False
+                break
+    
+    return TF_numbers
 
 def sum_of_odds(numbers):
     s = 0
-    for number in numbers:
-        if number % 2 == 1: 
-            s += number
+    if is_numbers(numbers):
+        for number in numbers:
+            if number % 2 == 1: 
+                s += number
+    else:
+        s = None
     return s
 
 
-assert sum_of_odds([1,1,2,9,1]) == 5
+assert sum_of_odds([1,1,2,9,1]) == 12
 assert sum_of_odds([1,2,10,10,3]) != 10
 assert sum_of_odds([1,2,3,4,"a","b","c"]) == None
--- problem5.py
+++ problem5.py
@@ -1,15 +1,38 @@
 # Problem 5
 # Write a function that identifies if there are common numbers between a pair of lists
 # Return True if there is, else return False
-
-
+def is_numbers(numbers):
+    """check whether the input is a list of numbers
+    input(list or tuple): a list of numbers
+    output(bool): if the type of the input is correct, return True; otherwise, return False
+    """
+    TF_numbers = False
+    # if the type of the input is a list and elements in the list are all numbers
+    if type(numbers) == list or type(numbers) == tuple:
+        TF_numbers = True
+        for number in numbers:
+            if type(number) != int and type(number) != float:
+                TF_numbers = False
+                break
+    
+    return TF_numbers
 
 def common_numbers(a,b):
     """Docstring here"""
     #
     # Your code here
     #
-    return __a_bool___
+    if is_numbers(a) and is_numbers(b):
+        if len(a) > len(b):
+            a, b = b, a
+        output = False
+        for element in a: 
+            if element in b:
+                output = True 
+                break
+    else: output = None
+    
+    return output
   
   
 
